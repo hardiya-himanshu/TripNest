db03108b5a1f374e17fd8a1726f7385bf8c17eb3
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2024-03-01 19:16:15 +0530

    create backend and frontend folders

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/login.css'
 import '../css/style.css'
 import loginimg from '../assets/login-signup-bg.jpg'
 import back from '../assets/back.png'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 
 function Login() {
+
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+
+  async function submit(e){
+      e.preventDefault()
+      try{
+          await axios.post("http://localhost:5174/login",{
+            email, password
+          })
+      }
+      catch{
+          console.log(e);
+      }
+  }
+
   return (
     <div className='main'>
     <img className='bg_login' src={loginimg} alt="" />
@@ -20,14 +37,15 @@ function Login() {
             LOGIN
         </div>
         
-        <form>
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
+        <form action='POST' onSubmit={submit}>
+            <input type="email" placeholder='Email' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+            <input type="password" placeholder='Password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
             <div className="remember">
                 <label htmlFor="remember">Remember Me</label>
                 <input type="checkbox" name="remember" id="remember" />
             </div>
-            <Link to="/" className='w-fit relative left-0 right-0 m-auto'><button className='login_btn'>LOGIN</button></Link>
+            <Link to="/" className='w-fit relative left-0 right-0 m-auto'><button type="submit" className='login_btn' >LOGIN</button>
+            </Link>
             <Link to='/signup'>
             <div className="gotologin font-bold underline cursor-pointer">Sign Up?</div>
             </Link>
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -6,6 +6,25 @@ import back from '../assets/back.png'
 import {Link} from 'react-router-dom'
 
 function Signup() {
+
+  const[username, setUsername] = useState("")
+  const[email, setEmail] = useState("")
+  const[password, setPassword] = useState("")
+
+
+  async function submit(e){
+    e.preventDefault()
+    try{
+        await axios.post("http://localhost:5174/signup",{
+          username, email, password
+        })
+    }
+    catch{
+        console.log(e);
+    }
+  }
+
+
   return (
     <>
       <div className='main'>
@@ -18,15 +37,16 @@ function Signup() {
             SIGN UP
         </div>
 
-        <form>
-            <input type="name" placeholder='Name' />
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
+        <form action='POST' onSubmit={submit}>
+            <input type="name" placeholder='Name' value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+            <input type="email" placeholder='Email'  value={email} onChange={(e)=>{setEmail(e.target.value)}} />
+            <input type="password" placeholder='Password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
             <div className="t_c">
                 <label htmlFor="t_c">I accept the Terms & Conditions</label>
                 <input type="checkbox" name="t_c" id="t_c" />
             </div>
-            <Link to="/login" className='w-fit relative left-0 right-0 m-auto'><button className='signup_btn'>SIGN UP</button></Link>
+            <Link to="/login" className='w-fit relative left-0 right-0 m-auto'><button type='submit' className='signup_btn'>SIGN UP</button>
+            </Link>
             <Link to="/login" >
             <div className="gotologin font-bold underline cursor-pointer">Login?</div>
 
